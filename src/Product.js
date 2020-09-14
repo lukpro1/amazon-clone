2de4538b5755411ad1b17763549f52d747a159f1
Lukasz <lukpro.dev@gmail.com>
2020-09-14 11:52:03 +0200

    finish product component

--- src/Product.js
+++ src/Product.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import './styles/Product.scss'
 
-function Product() {
+function Product({ id, title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The Lean Startup</p>
+        <p>{title}</p>
         <p className="product_price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
-          <p>⭐</p>
-          <p>⭐</p>
-          <p>⭐</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p><span>⭐</span></p>
+            ))}
         </div>
       </div>
 
-      <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" alt=""/>
+      <img src={image} alt=""/>
 
       <button>Add to Basket</button>
     </div>
